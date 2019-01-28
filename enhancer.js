2dcfa7abebdd3486d96a4dfeeb8b7705fec7fe19
Civoremo <omerovic_n@hotmail.com>
2019-01-28 18:01:52 -0500

    success and fail worked on

--- enhancer.js
+++ enhancer.js
@@ -2,13 +2,86 @@ module.exports = {
     success,
     fail,
     repair,
+    // enhance,
 };
 
 function success(item) {
+    if (item.enhancement >= 0) {
+        if (
+            (item.enhancement <= 5 && item.type === "armor") ||
+            (item.enhancement <= 7 && item.type === "weapon")
+        ) {
+            item.enhancement += 1;
+            item.name = `[+${item.enhancement}] ${item.originalName}`;
+        } else if (
+            (item.enhancement > 5 && item.type === "armor") ||
+            (item.enhancement > 7 && item.type === "weapon")
+        ) {
+            item.enhancement += 1;
+            switch (item.enhancement) {
+                case 16:
+                    item.name = `[PRI] ${item.originalName}`;
+                    break;
+                case 17:
+                    item.name = `[DUO] ${item.originalName}`;
+                    break;
+                case 18:
+                    item.name = `[TRI] ${item.originalName}`;
+                    break;
+                case 19:
+                    item.name = `[TET] ${item.originalName}`;
+                    break;
+                case 20:
+                    item.name = `[PEN] ${item.originalName}`;
+                    break;
+                default:
+                    if (item.enhancement !== 0) {
+                        item.name = `[+${item.enhancement}] ${
+                            item.originalName
+                        }`;
+                    } else {
+                        item.name = `${item.originalName}`;
+                    }
+            }
+        }
+    }
+    // console.log(item);
     return item;
 }
 
 function fail(item) {
+    if (item.enhancement > 0 && item.enhancement < 14) {
+        item.enhancement -= 5;
+    }
+    if (item.enhancement > 14) {
+        item.enhancement -= 10;
+    }
+    switch (item.enhancement) {
+        case 16:
+            item.name = `[PRI] ${item.originalName}`;
+            break;
+        case 17:
+            item.name = `[DUO] ${item.originalName}`;
+            break;
+        case 18:
+            item.name = `[TRI] ${item.originalName}`;
+            break;
+        case 19:
+            item.name = `[TET] ${item.originalName}`;
+            break;
+        case 20:
+            item.name = `[PEN] ${item.originalName}`;
+            break;
+        default:
+            if (item.enhancement > 0) {
+                item.name = `[+${item.enhancement}] ${item.originalName}`;
+            }
+            if (item.enhancement <= 0) {
+                item.enhancement = 0;
+                item.name = `${item.originalName}`;
+            }
+    }
+    console.log(item);
     return item;
 }
 
@@ -16,3 +89,7 @@ function repair(item) {
     item.durability = 100;
     return item;
 }
+
+// function enhance(item) {
+//     return item;
+// }
--- enhancer.spec.js
+++ enhancer.spec.js
@@ -1,17 +1,122 @@
 const enhancer = require("./enhancer.js");
 
 describe("SUCCESS", () => {
-    test("success", () => {
-        expect(enhancer.success({ name: "sword" })).toEqual({ name: "sword" });
+    const weapon = {
+        originalName: "Flawless Sword",
+        name: "Flawless Sword",
+        type: "weapon",
+        durability: 90,
+        enhancement: 9,
+    };
+
+    const weaponEnh = {
+        originalName: "Flawless Sword",
+        name: "[+10] Flawless Sword",
+        type: "weapon",
+        durability: 90,
+        enhancement: 10,
+    };
+
+    const armor = {
+        originalName: "Shiny Boots",
+        name: "[+15] Shiny Boots",
+        type: "armor",
+        durability: 80,
+        enhancement: 15,
+    };
+
+    const armorEnh = {
+        originalName: "Shiny Boots",
+        name: "[PRI] Shiny Boots",
+        type: "armor",
+        durability: 80,
+        enhancement: 16,
+    };
+
+    test("success weapon", () => {
+        expect(enhancer.success(weapon)).toEqual(weaponEnh);
+    });
+
+    test("success armor", () => {
+        expect(enhancer.success(armor)).toEqual(armorEnh);
     });
 });
 
 describe("FAIL", () => {
+    const weapon = {
+        originalName: "Flawless Sword",
+        name: "Flawless Sword",
+        type: "weapon",
+        durability: 90,
+        enhancement: 3,
+    };
+
+    const weaponEnh = {
+        originalName: "Flawless Sword",
+        name: "Flawless Sword",
+        type: "weapon",
+        durability: 90,
+        enhancement: 0,
+    };
+
+    const armor = {
+        originalName: "Shiny Boots",
+        name: "[+15] Shiny Boots",
+        type: "armor",
+        durability: 80,
+        enhancement: 15,
+    };
+
+    const armorEnh = {
+        originalName: "Shiny Boots",
+        name: "[PRI] Shiny Boots",
+        type: "armor",
+        durability: 80,
+        enhancement: 16,
+    };
+
     test("failed enhancement", () => {
-        expect(enhancer.fail({ name: "sword" })).toEqual({ name: "bow" });
+        expect(enhancer.fail(weapon)).toEqual(weaponEnh);
     });
 });
 
-describe("REPAIR", () => {
-    expect(enhancer.repair({ durability: 50 })).toEqual({ durability: 100 });
-});
+// describe("REPAIR", () => {
+//     const weapon = {
+//         originalName: "Flawless Sword",
+//         name: "Flawless Sword",
+//         type: "weapon",
+//         durability: 90,
+//         enhancement: 0,
+//     };
+
+//     const weaponEnh = {
+//         originalName: "Flawless Sword",
+//         name: "Flawless Sword",
+//         type: "weapon",
+//         durability: 100,
+//         enhancement: 0,
+//     };
+
+//     const armor = {
+//         originalName: "Shiny Boots",
+//         name: "Shiny Boots",
+//         type: "armor",
+//         durability: 80,
+//         enhancement: 0,
+//     };
+
+//     const armorEnh = {
+//         originalName: "Shiny Boots",
+//         name: "Shiny Boots",
+//         type: "armor",
+//         durability: 100,
+//         enhancement: 0,
+//     };
+
+//     test("repair weapon", () => {
+//         expect(enhancer.repair(weapon)).toEqual(weaponEnh);
+//     });
+//     test("repair armor", () => {
+//         expect(enhancer.repair(armor)).toEqual(armorEnh);
+//     });
+// });
